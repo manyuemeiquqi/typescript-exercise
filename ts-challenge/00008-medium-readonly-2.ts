3143a9cc76e37ba1165c1ce378f40b02942b6b28
jinpeng <940495614@qq.com>
2023-08-03 19:26:29 +0800

    feat: readonly

--- ts-challenge/00008-medium-readonly-2.ts
+++ ts-challenge/00008-medium-readonly-2.ts
@@ -31,6 +31,5 @@ interface Expected {
 
 
 // ============= Your Code Here =============
-type MyReadonly2<T, K> = {
-   [Key in keyof T as Key extends K? Key:Key]:T[Key]
-}
+// 这里可以思考下 | 和 & 运算， |为 或 要么 A 要么 B 如果AB为interface 也是一样的，&为去交集，如果 AB为interface 可以聚合在一起，但是有冲突key，会推断出never
+type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T,K> & Readonly<Pick<T,K>>
