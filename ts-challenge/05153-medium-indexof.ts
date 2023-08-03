a0d73b4178446165c82a55f0946b8be51b937f2b
jinpeng <940495614@qq.com>
2023-08-03 19:28:17 +0800

    feat: indexof

---
+++ ts-challenge/05153-medium-indexof.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<IndexOf<[1, 2, 3], 2>, 1>>,
+  Expect<Equal<IndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 2>>,
+  Expect<Equal<IndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<IndexOf<[string, 1, number, 'a'], number>, 2>>,
+  Expect<Equal<IndexOf<[string, 1, number, 'a', any], any>, 4>>,
+  Expect<Equal<IndexOf<[string, 'a'], 'a'>, 1>>,
+  Expect<Equal<IndexOf<[any, 1], 1>, 1>>,
+]
+
+
+// ============= Your Code Here =============
+// 需要加参数 记录
+type IndexOf<T, U, Index extends any[] = []> = T extends [infer First, ...infer Rest] ?
+  Equal<First, U> extends true ? Index['length'] : IndexOf<Rest, U, [...Index, First]> : -1
+
