49b82c2d83781a17414f8a24a554ce41a219b0d1
jiangyeka <940495614@qq.com>
2023-08-09 22:26:09 +0800

    feat: union to tuple

---
+++ ts-challenge/00730-hard-union-to-tuple.ts
@@ -0,0 +1,52 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type ExtractValuesOfTuple<T extends any[]> = T[keyof T & number]
+
+type cases = [
+  Expect<Equal<UnionToTuple<'a' | 'b'>['length'], 2>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a' | 'b'>>, 'a' | 'b'>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a'>>, 'a'>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any>>, any>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<undefined | void | 1>>, void | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any | 1>>, any | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any | 1>>, any>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'d' | 'f' | 1 | never>>, 'f' | 'd' | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<[{ a: 1 }] | 1>>, [{ a: 1 }] | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<never>>, never>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a' | 'b' | 'c' | 1 | 2 | 'd' | 'e' | 'f' | 'g'>>, 'f' | 'e' | 1 | 2 | 'g' | 'c' | 'd' | 'a' | 'b'>>,
+]
+
+
+// ============= Your Code Here =============
+// type UnionToTuple<T> =   
+
+type h = ((x: 1) => 0) & ((x: 2) => 0) //why h not never
+let a:h = (x:1|2)=>0
+
+/**
+ * UnionToIntersection<{ foo: string } | { bar: string }> =
+ *  { foo: string } & { bar: string }.
+ */
+type UnionToIntersection<U> = (
+  U extends unknown ? (arg: U) => 0 : never
+) extends (arg: infer I) => 0
+  ? I
+  : never;
+
+/**
+ * LastInUnion<1 | 2> = 2.
+ */
+type LastInUnion<U> = UnionToIntersection<
+  U extends unknown ? (x: U) => 0 : never
+> extends (x: infer L) => 0
+  ? L
+  : never;
+let b:UnionToIntersection<1|2|3>
+let c:LastInUnion<1|2|3>
+/**
+ * UnionToTuple<1 | 2> = [1, 2].
+ */
+type UnionToTuple<U, Last = LastInUnion<U>> = [U] extends [never]
+  ? []
+  : [...UnionToTuple<Exclude<U, Last>>, Last];
