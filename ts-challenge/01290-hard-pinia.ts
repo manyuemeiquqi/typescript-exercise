cb731b59b9c333dd45cdecab574df29dfdda3707
jinpeng <940495614@qq.com>
2023-08-09 17:50:38 +0800

    feat: pinia

---
+++ ts-challenge/01290-hard-pinia.ts
@@ -0,0 +1,82 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const store = defineStore({
+  id: '',
+  state: () => ({
+    num: 0,
+    str: '',
+  }),
+  getters: {
+    stringifiedNum() {
+      // @ts-expect-error
+      this.num += 1
+
+      return this.num.toString()
+    },
+    parsedNum() {
+      return parseInt(this.stringifiedNum)
+    },
+  },
+  actions: {
+    init() {
+      this.reset()
+      this.increment()
+    },
+    increment(step = 1) {
+      this.num += step
+    },
+    reset() {
+      this.num = 0
+
+      // @ts-expect-error
+      this.parsedNum = 0
+
+      return true
+    },
+    setNum(value: number) {
+      this.num = value
+    },
+  },
+})
+
+// @ts-expect-error
+store.nopeStateProp
+// @ts-expect-error
+store.nopeGetter
+// @ts-expect-error
+store.stringifiedNum()
+store.init()
+// @ts-expect-error
+store.init(0)
+store.increment()
+store.increment(2)
+// @ts-expect-error
+store.setNum()
+// @ts-expect-error
+store.setNum('3')
+store.setNum(3)
+const r = store.reset()
+
+type _tests = [
+  Expect<Equal<typeof store.num, number>>,
+  Expect<Equal<typeof store.str, string>>,
+  Expect<Equal<typeof store.stringifiedNum, string>>,
+  Expect<Equal<typeof store.parsedNum, number>>,
+  Expect<Equal<typeof r, true>>,
+]
+
+
+// ============= Your Code Here =============
+
+
+declare function defineStore<S,G,A>(store: Store<S,G,A>): A&S&RemapGetter<G>
+type RemapGetter<T> = {
+  [P in keyof T]: T[P] extends ()=> infer R ? R :never
+}
+type Store<State,G,A> ={
+  state:()=>State
+  getters:G&ThisType<Readonly<State>&RemapGetter<G>>
+  actions:ThisType<State&A>&A,
+  id:string
+}
