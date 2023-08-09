7dd0fbeb3c8d7b2d6e29bba497f63a7807a86d05
jiangyeka <940495614@qq.com>
2023-08-09 23:28:16 +0800

    feat: string to number

---
+++ ts-challenge/00300-hard-string-to-number.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<ToNumber<'0'>, 0>>,
+  Expect<Equal<ToNumber<'5'>, 5>>,
+  Expect<Equal<ToNumber<'12'>, 12>>,
+  Expect<Equal<ToNumber<'27'>, 27>>,
+  Expect<Equal<ToNumber<'18@7_$%'>, never>>,
+]
+
+
+// ============= Your Code Here =============
+// type ToNumber<S extends string> = any
+// your answers
+type ToNumber<S extends string> = S extends `${infer X extends number}` ? X : never;
+
