aadadc8ca735ae3d39275907bdc36f032cce5977
jinpeng <940495614@qq.com>
2023-08-10 11:18:40 +0800

    feat: C printf

---
+++ ts-challenge/00147-hard-c-printf-parser.ts
@@ -0,0 +1,30 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<ParsePrintFormat<''>, []>>,
+  Expect<Equal<ParsePrintFormat<'Any string.'>, []>>,
+  Expect<Equal<ParsePrintFormat<'The result is %d.'>, ['dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %%d.'>, []>>,
+  Expect<Equal<ParsePrintFormat<'The result is %%%d.'>, ['dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %f.'>, ['float']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %h.'>, ['hex']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %q.'>, []>>,
+  Expect<Equal<ParsePrintFormat<'Hello %s: score is %d.'>, ['string', 'dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %'>, []>>,
+]
+
+
+// ============= Your Code Here =============
+type ControlsMap = {
+  c: 'char'
+  s: 'string'
+  d: 'dec'
+  o: 'oct'
+  h: 'hex'
+  f: 'float'
+  p: 'pointer'
+}
+
+type ParsePrintFormat<T, E extends any[] = []> = T extends `${infer A}%${infer B}` ? `${B}` extends `${infer C}${infer D}` ?
+  C extends keyof ControlsMap ? ParsePrintFormat<D, [...E, ControlsMap[C]]> : ParsePrintFormat<D, [...E]> : E : E
