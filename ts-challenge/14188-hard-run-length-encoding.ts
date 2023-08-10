9d9ad9f17c66e0438875b14aabe8a395cbe51479
jinpeng <940495614@qq.com>
2023-08-10 16:23:29 +0800

    feat: run length encoding

---
+++ ts-challenge/14188-hard-run-length-encoding.ts
@@ -0,0 +1,37 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  // Raw string -> encoded string
+  Expect<Equal<RLE.Encode<'AAABCCXXXXXXY'>, '3AB2C6XY'>>,
+
+  // Encoded string -> decoded string
+  Expect<Equal<RLE.Decode<'3AB2C6XY'>, 'AAABCCXXXXXXY'>>,
+]
+
+
+// ============= Your Code Here =============
+namespace RLE {
+  type EncodeHelper<S extends string, P extends string = '', L extends 1[] = []> = S extends `${infer F}${infer R}`
+    ? F extends P
+      ? EncodeHelper<R, P, [1, ...L]>
+      : P extends ''
+        ? EncodeHelper<R, F, [1]>
+        : L['length'] extends 1
+          ? `${P}${EncodeHelper<R, F, [1]>}`
+          : `${L['length']}${P}${EncodeHelper<R, F, [1]>}`
+    : P extends ''
+      ? ''
+      : L['length'] extends 1
+        ? P
+        : `${L['length']}${P}`
+  export type Encode<S extends string> = EncodeHelper<S>
+
+  type DecodeHelper<S extends string, L extends string = ''> = S extends `${infer F}${infer R}`
+    ? F extends `${number}`
+      ? DecodeHelper<R, `${L}${F}`>
+      : `${Repeat<F, L extends '' ? '1' : L>}${DecodeHelper<R, ''>}`
+    : ''
+  type Repeat<S extends string, L extends string, C extends 1[] = []> = `${C['length']}` extends L ? '' : `${S}${Repeat<S, L, [1, ...C]>}`
+  export type Decode<S extends string> = DecodeHelper<S>
+}
